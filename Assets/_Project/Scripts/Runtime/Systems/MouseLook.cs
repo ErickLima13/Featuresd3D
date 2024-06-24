fbdc650e5fbf058e450faf6cdd0397a7373de57e
Erick Araujo <erickequipe7@gmail.com>
2024-06-24 17:14:14 -0300

    Start course

---
+++ Assets/_Project/Scripts/Runtime/Systems/MouseLook.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+
+    public float mouseSensitivity;
+    
+    public float minRotX, maxRotX;
+
+    public Transform playerBody;
+
+    private float xRotation;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, minRotX, maxRotX);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+
+}
